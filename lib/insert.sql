78e5e3d314c4b58792e192a5037c91e07d561627
Madison Evans <m.corbinevans@gmail.com>
2023-02-28 14:43:00 -0700

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+  ('Mr. Chocolate', 5, 'M', 'brown', 'friendly', true),
+  ('Rowdy', 3, 'M', 'black', 'energetic', true),
+  ('Tabitha', 7, 'F', 'gray', 'calm', true),
+  ('Sergeant Brown', 4, 'M', 'brown', 'disciplined', true),
+  ('Melissa', 2, 'F', 'blonde', 'shy', true),
+  ('Grinch', 6, 'M', 'green', 'grumpy', true),
+  ('Wendy', 1, 'F', 'white', 'playful', true),
+  (NULL, NULL, NULL, NULL, NULL, false);
